dee991f20c99af71234c225d29525f589f75c5ab
mdiktushar <md.tushar1804@gmail.com>
2025-05-04 16:01:40 +0600

    otp range update

--- app/Repositories/Api/V1/Auth/OTPRepository.php
+++ app/Repositories/Api/V1/Auth/OTPRepository.php
@@ -33,7 +33,7 @@ class OTPRepository implements OTPRepositoryInterface
             $user->otps()->whereOperation($operation)->delete();
 
             // Generate a new OTP
-            $otp = mt_rand(111111, 999999);
+            $otp = mt_rand(1111, 9999);
 
             // Store OTP in database
             $user->otps()->create([
