fa81eeb674c2c7a011ce56991df70702c5a7acda
mdiktushar <md.tushar1804@gmail.com>
2025-05-04 17:07:25 +0600

    fix otp

--- app/Repositories/Api/V1/Auth/OTPRepository.php
+++ app/Repositories/Api/V1/Auth/OTPRepository.php
@@ -33,8 +33,8 @@ class OTPRepository implements OTPRepositoryInterface
             $user->otps()->whereOperation($operation)->delete();
 
             // Generate a new OTP
-            $otp = mt_rand(1111, 9999);
-
+            // $otp = mt_rand(1111, 9999);
+            $otp = 1234;
             // Store OTP in database
             $user->otps()->create([
                 'operation' => $operation,
