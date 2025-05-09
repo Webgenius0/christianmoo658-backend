3a86b012274e9de297ab55a13bc6e59348ae4d30
mdiktushar <md.tushar1804@gmail.com>
2025-05-09 16:34:35 +0600

    course list

---
+++ app/Services/Api/V1/Course/CourseService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services\Api\V1\Course;
+
+use App\Interfaces\V1\Course\CourseRepositoryInterface;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Log;
+
+class CourseService
+{
+    /**
+     * courseRepository
+     * @var CourseRepositoryInterface
+     */
+    private CourseRepositoryInterface $courseRepository;
+
+    /**
+     * construct
+     * @param \App\Interfaces\V1\Course\CourseRepositoryInterface $courseRepository
+     */
+    public function __construct(CourseRepositoryInterface $courseRepository)
+    {
+        $this->courseRepository = $courseRepository;
+    }
+
+    /**
+     * getCourseList
+     * @return \Illuminate\Database\Eloquent\Collection<int, \App\Models\Course>
+     */
+    public function getCourseList(): Collection
+    {
+        try {
+            return $this->courseRepository->getAllCourses();
+        }catch(Exception $e) {
+            Log::error('CourseService::getCourseList', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+            throw $e;
+        }
+    }
+}
