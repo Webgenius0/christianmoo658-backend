5d210f19647405d09ae3188019d57b6761a3d2a2
mdiktushar <md.tushar1804@gmail.com>
2025-05-09 16:47:02 +0600

    get courses

--- routes/api/api.php
+++ routes/api/api.php
@@ -6,4 +6,7 @@ use Illuminate\Support\Facades\Route;
 /**
  * V1 API Routes:
  */
-require 'v1/auth/auth.php';                        // All Auth routes
+Route::prefix('v1')->group(function () {
+    require 'v1/auth/auth.php';                        // All Auth routes
+    require 'v1/course/course.php';                    // All Auth routes
+});
---
+++ routes/api/v1/course/course.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\V1\Course\CourseController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('course')->middleware(['auth:api', 'verified.api'])->controller(CourseController::class)->group(function () {
+    Route::get('/', 'index');
+});
