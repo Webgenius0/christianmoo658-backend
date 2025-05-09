da058ff498f5392ee0ed1ad3a07d21a9993b6127
mdiktushar <md.tushar1804@gmail.com>
2025-05-09 10:03:58 +0600

    plan model

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,5 +6,37 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'price',
+        'duration',
+        'duration_value',
+        'features',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'price'          => 'double',
+            'duration_value' => 'integer',
+            'features'       => 'json',
+            'created_at'     => 'datetime',
+            'updated_at'     => 'datetime',
+        ];
+    }
 }
