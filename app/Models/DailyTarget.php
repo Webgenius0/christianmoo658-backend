d8599317966a55161bd8e292777600d6866aae69
mdiktushar <md.tushar1804@gmail.com>
2025-05-09 10:14:27 +0600

    daily target name fix

--- app/Models/DailyTarget.php
+++ app/Models/DailyTarget.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class DalyTarget extends Model
+class DailyTarget extends Model
 {
     /**
      * fillable
--- database/migrations/2025_05_05_084359_create_daily_targets_table.php
+++ database/migrations/2025_05_05_084359_create_daily_targets_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('daly_targets', function (Blueprint $table) {
+        Schema::create('daily_targets', function (Blueprint $table) {
             $table->id();
             $table->string('time');
             $table->timestamps();
