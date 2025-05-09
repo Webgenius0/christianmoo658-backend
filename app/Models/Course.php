6bc46e170fd2be4f75b79c433caf8551e4506a28
mdiktushar <md.tushar1804@gmail.com>
2025-05-09 10:10:56 +0600

    assingment

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    /**
+     * fillable
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at'     => 'datetime',
+            'updated_at'     => 'datetime',
+        ];
+    }
 }
