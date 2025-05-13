620dde9471d5e0d5e31cb039d6800b79af6221af
mdiktushar <md.tushar1804@gmail.com>
2025-05-13 11:22:29 +0600

    get list

--- app/Services/Api/V1/DailyTarget/DailyTargetService.php
+++ app/Services/Api/V1/DailyTarget/DailyTargetService.php
@@ -2,7 +2,43 @@
 
 namespace App\Services\Api\V1\DailyTarget;
 
+use App\Interfaces\V1\DailyTarget\DailyTargetRepositoryInterface;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Log;
+
 class DailyTargetService
 {
-    
+    /**
+     * dailyTargetRepository
+     * @var DailyTargetRepositoryInterface
+     */
+    protected DailyTargetRepositoryInterface $dailyTargetRepository;
+
+    /**
+     * construct
+     * @param \App\Interfaces\V1\DailyTarget\DailyTargetRepositoryInterface $dailyTargetRepository
+     */
+    public function __construct(DailyTargetRepositoryInterface $dailyTargetRepository)
+    {
+        $this->dailyTargetRepository = $dailyTargetRepository;
+    }
+
+    /**
+     * getDailyTargetList
+     * @return \Illuminate\Database\Eloquent\Collection<int, \App\Models\DailyTarget>
+     */
+    public function getDailyTargetList():Collection
+    {
+        try {
+            return $this->dailyTargetRepository->getAllDailyTarget();
+        }catch(Exception $e) {
+            Log::error('DailyTargetService::getDailyTargetList', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+            throw $e;
+        }
+    }
+
 }
