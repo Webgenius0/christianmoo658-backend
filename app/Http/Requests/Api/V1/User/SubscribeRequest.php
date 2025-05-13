9b3e5e7a0be88469a6c94359741e7811270ed1f4
mdiktushar <md.tushar1804@gmail.com>
2025-05-13 12:45:06 +0600

    subscribeRequest created

---
+++ app/Http/Requests/Api/V1/User/SubscribeRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\User;
+
+use App\Traits\V1\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class SubscribeRequest extends FormRequest
+{
+    use ApiResponse;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'course_id' => 'required|exists:courses,id',
+            'daily_target_id' => 'required|exists:daily_targets,id',
+        ];
+    }
+
+    /**
+     * failedValidation
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @throws \Illuminate\Validation\ValidationException
+     * @return never
+     */
+    protected function failedValidation(Validator $validator): never
+    {
+        $fieldsToCheck = ['email', 'password'];
+        $message = 'Validation error';
+
+        foreach ($fieldsToCheck as $field) {
+            $errors = $validator->errors()->get($field);
+            if (!empty($errors)) {
+                $message = $errors[0];
+                break;
+            }
+        }
+
+        $response = $this->error(
+            422,
+            $message,
+            $validator->errors(),
+        );
+
+        throw new ValidationException($validator, $response);
+    }
+}
